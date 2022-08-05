6d84bdc8c3d2e50aa1370b1605e817f91df4cdd4
LJS0000 <jisu2281l@naver.com>
2022-08-05 17:50:12 +0900

    스타일드 컴포넌츠, 리덕스, 라우터, 나노id 패키지설치

---
+++ src/shared/Router.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//pages
+import Detail from '../pages/Detail'
+import Edit from '../pages/Edit'
+import Login from '../pages/Login'
+import Main from '../pages/Main'
+import MyPage from '../pages/MyPage'
+import Post from '../pages/Post'
+import SignUp from '../pages/SignUp'
+
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="detail" element={<Detail/>}/>
+        <Route path="edit" element={<Edit/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="/" element={<Main/>}/>
+        <Route path="mypage" element={<MyPage/>}/>
+        <Route path="post" element={<Post/>}/>
+        <Route path="signup" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;

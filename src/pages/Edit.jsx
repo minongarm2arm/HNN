9a46404a38157aaabf62a7f42d0d03017ff97039
LJS0000 <jisu2281l@naver.com>
2022-08-06 01:48:53 +0900

    post&edit 페이지 뷰 작업 #1

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,11 +1,71 @@
 import React from "react";
 
-const Edit = () => {
+//package
+import styled from "styled-components";
+
+//assets
+import placeholder from '../src_assets/placeholder.png'
+
+
+const Post = () => {
   return (
-    <h2>
-      에딧
-    </h2>
+    <StPostBox>
+      <StPostImg src={placeholder} alt='사진 업로드'/>
+      <button>사진 업로드</button>
+      <StPostForm>
+        <StPostInput 
+          id="음식이름"
+          placeholder="음식 이름"
+          type="text"
+        />
+        <StPostInput 
+          id="식당이름"
+          placeholder="식당 이름"
+          type="text"
+        />
+        <StPostInput 
+          id="식당위치"
+          placeholder="식당 위치"
+          type="text"
+        />
+        <textarea
+          cols={43} rows={4}
+          placeholder='후기'  
+        />
+        <StPostInput 
+          id="별점"
+          placeholder="별점"
+          type="text"
+        />
+      </StPostForm>
+      <button>저장</button>
+    </StPostBox>
   )
 }
 
-export default Edit
+export default Post
+
+const StPostBox = styled.div`
+width: 700px;
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 50px auto;
+`
+
+const StPostImg = styled.img`
+width: 300px;
+margin: 10px;
+`
+
+const StPostInput = styled.input`
+width: 300px;
+height: 30px;
+margin: 5px;
+`
+
+const StPostForm = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 
+//package
+import styled from "styled-components";
+
+//assets
+import placeholder from '../src_assets/placeholder.png'
+
+
 const Post = () => {
   return (
-    <h2>
-      포스트
-    </h2>
+    <StPostBox>
+      <StPostImg src={placeholder} alt='사진 업로드'/>
+      <button>사진 업로드</button>
+      <StPostContent>
+        <h3>음식 이름: 떡볶이</h3>
+        <h3>식당 이름: 웃긴 떡볶이</h3>
+        <h3>식당 위치: 서대문</h3>
+        <h3>후기: 둘이 먹다 하나가 죽으면 알아차릴 맛입니다.</h3>
+        <h3>별점: ★★★☆☆</h3>
+      </StPostContent>
+      <button>수정하기</button>
+    </StPostBox>
   )
 }
 
 export default Post
+
+const StPostBox = styled.div`
+width: 700px;
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 50px auto;
+`
+
+const StPostImg = styled.img`
+width: 300px;
+margin: 10px;
+`
+
+const StPostContent = styled.div`
+width: 450px;
+height: 200px;
+border: 1px solid black;
+margin: 10px;
+`

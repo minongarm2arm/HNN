aa19afd6a9583132c1b9a939be9bb2073a927dfa
chasura416 <chasura416@naver.com>
2022-08-06 15:37:17 +0900

    main first commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import styled from "styled-components"
+import CommonHeader from "../components/CommonHeader"
+import Layout from "../components/Layout"
 
 const Main = () => {
 
   return (
-    <h2>
-      메인
-    </h2>
+    <Layout>
+      <CommonHeader />
+      <h2>메인</h2>
+    </Layout>
   )
 }
 

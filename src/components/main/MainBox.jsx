58057cb6ae552c7e63543f8865024eec021ad738
chasura416 <chasura416@naver.com>
2022-08-11 13:34:43 +0900

    포스팅 정렬 최신화

--- src/components/main/MainBox.jsx
+++ src/components/main/MainBox.jsx
@@ -1,54 +1,109 @@
-import React from "react";
+import * as React from "react";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import placeholder from "../../src_assets/placeholder.png";
 import { useNavigate } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import createRoot from 'react-dom/client';
+// import { Virtuoso } from 'react-virtuoso';
+// import { useInView } from "react-intersection-observer";
 
 const MainBox = () => {
-    
-  const [posts, setPosts] = useState();
+  
+  // const [ref, inView] =  useInView();
+  // const [page, setPage] = useState();
+  // const [loading, setLoading] = useState(false);
+  // const [item, setItems] = useState([]);
+
+  const [posts, setPosts] = useState([]);
   const navigate = useNavigate();
 
   const fetchPosts = async () => {
     const {data} = await axios.get('http://localhost:3001/posts');
     setPosts(data);
   };
+
+//   const fetchPosts = useCallback(async () => {
+//     setLoading(true)
+//     await axios.get(`${'http://localhost:3001/posts'}/page=${page}`).then((res) => {
+//       setPosts(prevState => [...prevState, res])
+//     })
+//     setLoading(false)
+//   }, [page])
+// console.log(fetchPosts)
+
+  // const getItems = (async () => {
+  //   setLoading(true)
+  //   await axios.get(`${'http://localhost:3001/posts'}/page=${page}`).then((res) => {
+  //     setItems(prevState => [...prevState, res])
+  //   })
+  //   setLoading(false)
+  // }, [page])
   
-  console.log(posts)
+  // useEffect(() => {
+  //   getItems()
+  // }, [getItems]);
+
+  // useEffect(() => {
+  //   // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
+  //   if (inView && !loading) {
+  //     setPage(prevState => prevState + 1)
+  //   }
+  // }, [inView, loading]);
 
   useEffect(() => {
     fetchPosts()
   }, []);
+
+  // useEffect(() => {
+  //   fetchPosts()
+  // }, [fetchPosts]);
+
+//   <Virtuoso
+//   style={{ height: '400px' }}
+//   totalCount={200}
+//   itemContent={index => <div>Item {index}</div>}
+// />
   
 
-  return (
-    <>
+return (
+  <>
     <MainBoxContainer>
-      {posts?.map((post) => (
-        <MainBoxLayout key={post.id} onClick={()=> {
-          navigate(`/detail/${post.id}`)
-        }}>
-        <StMainBox>
-          <StPostImg>
-            <img src={post.imgFile} alt='이미지 없어용' />
-          </StPostImg>
-          <StPostHover>
-              <div className="hoverText">
-                <p>음식명 : {post.food}</p>
-                <p>가게명 : {post.restaurant}</p>
-                <p>가게 위치: {post.location}</p>
-              </div>
-            </StPostHover>
-        </StMainBox>
-        <p>@ UserName</p>  
-        </MainBoxLayout>
-      ))}
+    {/* <Virtuoso
+      style={{ height: '400px' }}
+      totalCount={200}
+      itemContent={index => 
+       */}
+        {posts?.map((post, idx) => (
+            <MainBoxLayout key={post.id} onClick={()=> {
+              navigate(`/detail/${post.id}`);
+            }}>
+              <StMainBox>
+                <StPostImg>
+                  <img src={post.imgFile} alt='이미지 없어용' />
+                </StPostImg>
+                <StPostHover>
+                    <div className="hoverText">
+                      <p>음식명 : {post.food}</p>
+                      <p>가게명 : {post.restaurant}</p>
+                      <p>가게 위치: {post.location}</p>
+                    </div>
+                  </StPostHover>
+              </StMainBox>
+              <p>@ UserName</p>  
+            </MainBoxLayout>
+        )).reverse()}
+
+      {/* }/> */}
     </MainBoxContainer>
   </>
   )
   
 }
+
+// ReactDOM.root.render(<MainBox />, document.getElementById('root'))
+
 
 const MainBoxContainer = styled.div`
   display: flex;
